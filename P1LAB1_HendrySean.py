6b37144a244a318831c81c7ee9a8d67b25795eb2
SeanHendry17 <hendrys6580@student.faytechcc.edu>
2025-09-17 13:39:44 -0400

    Add files via upload

---
+++ P1LAB1_HendrySean.py
@@ -0,0 +1,9 @@
+#Sean Hendry
+#09/17/2025
+#P1LAB1
+#This assignment teaches the student how to create a Python program using Visual Studio Code. 
+#The goal is to get input from the user and display output.
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print("Hello,", first_name, last_name + "!","Welcome to CTI-110")
